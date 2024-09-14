c31cdd571de75360a85e30a4181af00fa35f688a
furrki <furkankaynary@gmail.com>
2024-09-14 16:30:08 +0300

    Implemented the script with a new target

--- SudokovGeneratorScript/main.swift
+++ SudokovGeneratorScript/main.swift
@@ -7,5 +7,29 @@
 
 import Foundation
 
-print("Hello, World!")
+let easyDepth = 45
+let mediumDepth = 35
+let hardDepth = 26
 
+func writeToFile(name: String, levels: [Level]) {
+    // documents directory
+    let fileURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent(name)
+    let data = try? JSONEncoder().encode(levels)
+    let dataString = data?.base64EncodedString()
+    do {
+        try dataString!.write(to: fileURL, atomically: false, encoding: .utf8)
+    }
+    catch {/* error handling here */}
+}
+
+var levels: [Level] = []
+var tableBuilder = TableBuilder()
+
+for _ in 0...99 {
+    tableBuilder = TableBuilder(depth: easyDepth)
+    let cellsToHide = tableBuilder.cellsToHide
+    levels.append(Level(table: tableBuilder.tableState, cellsToHide: cellsToHide))
+}
+writeToFile(name: "easy.data", levels: levels)
+
+levels = []
